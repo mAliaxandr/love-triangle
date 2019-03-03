232598c0bdaf0f44fcf955d99413ed311aaacbc2
Aliaksandr Mamchyts <al.mamchyts@gmail.com>
2019-03-03 23:21:44 +0300

    add code in index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+        for( var i = 0; i < preferences.length; i++){
+            var pech1 = preferences[i] - 1;
+            var pech2 = preferences[pech1] - 1;
+            var pech3 = preferences[pech2] - 1;
+            if(pech3 == i){
+                    preferences[pech1] = NaN;    
+                        if(pech1 != pech2 ){
+                            count +=1; 
+                        }
+                }
+        }
+        return count ;
 };
